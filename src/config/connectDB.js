b322edbee573af4a1b644f7cb8ababe028f9453b
Nile Chakraborty <nileprofessorofweb@gmail.com>
2024-06-14 21:40:22 +0530

    Connect To Database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: 'localhost',
-    user: 'tirangaofhasibul8w37',
-    password: 'LLbZ2YPp3E7DbpEZ',
-    database: 'tirangaofhasibul8w37',
+    user: 'root',
+    password: '',
+    database: 'tiranga',
 });
 
 export default connection;
