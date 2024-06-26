e5cd08f3710d1956f9e9d27f92f07786b5214d1a
Nile Chakraborty <nileprofessorofweb@gmail.com>
2024-06-27 01:48:21 +0530

    July 27

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: '46.4.38.214',
-    user: 'professorofweb_tirangagame',
-    password: 'professorofweb_tirangagame',
-    database: 'professorofweb_tirangagame',
+    user: 'professorofweb_tiranga',
+    password: 'professorofweb_tiranga',
+    database: 'professorofweb_tiranga',
 });
 
 export default connection;
